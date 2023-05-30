f544962e247fe42022f4669aa1c42c0c162e30a3
Yuan544 <134912347+Yuan544@users.noreply.github.com>
2023-05-30 10:04:03 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,110 @@
+const paragraph = document.getElementById('paragraph');
+const imgうざおじ = document.getElementById('imgうざおじ');
+const imgGu = document.getElementById('imgGu');
+const imgChoki = document.getElementById('imgChoki');
+const imgPa = document.getElementById('imgPa');
+const reStartButton = document.getElementById('reStart');
+const scoreButton = document.getElementById('score');
+const resultText = document.getElementById('result');
+const gameArea = document.getElementById('gameArea');
+
+let endFlag = false;
+let win = 0;
+let lose = 0;
+let draw = 0;
+let player = [];
+let うざおじ = [];
+let result = [];
+let handArray = ['グー', 'チョキ', 'パー'];
+let winOrLose = ['勝ち', '負け', 'あいこ'];
+
+function game(num){
+
+    if(endFlag){
+        return;
+      }
+      endFlag=true;
+    
+    paragraph.innerText = 'ポン！';
+
+    player.push(num);
+
+    if(num===0){
+        imgChoki.classList.add('hide');
+        imgPa.classList.add('hide');
+    }else if(num===1){
+        imgGu.classList.add('hide');
+        imgPa.classList.add('hide'); 
+    }else{
+        imgChoki.classList.add('hide');
+        imgGu.classList.add('hide');   
+    }
+
+    const randomNum = Math.floor(Math.random()*3);
+    うざおじ.push(randomNum);
+    
+    if(randomNum === 0){
+        imgうざおじ.src = 'img/グーおじ２.png';
+    }else if(randomNum === 1){
+        imgうざおじ.src = 'img/バツおじ２.png';
+    }else if(randomNum === 2){
+        imgうざおじ.src = 'img/パーおじ２.png';
+    }
+
+    if(num === randomNum){
+        resultText.innerText = 'きみぃ、0.1秒遅かったよね？それ不正だからな？次したら警察沙汰になるからな？まぁ僕ちんは優しいから今回は見逃すよ、次から気をつけろよ、この敗北者が！';
+        draw++;
+        result.push(2);
+    }else if(num === 0 && randomNum === 1){
+        resultText.innerText = 'チッ勝つのかよ、でも指の数ではグーよりチョキの方が多いからチョキの勝ち';
+        win++;
+        result.push(0);
+    }else if(num === 1 && randomNum === 2){
+        resultText.innerText = 'でもこっちが固い鉄の板だったらこっちの勝ちやから実質パーの勝ち';
+        win++;
+        result.push(0);
+    }else if(num === 2 && randomNum === 0){
+        resultText.innerText = '紙と石じゃ石の方が強いからグーの勝ち〜';
+        win++;
+        result.push(0);
+    }else{
+        resultText.innerText = '僕ちんに負けるとかダサすぎぃぃぃぃぃぃぃ⤴︎（ビブラート）、10年修行してから出直してきな笑🤗';
+        lose++;
+        result.push(1);
+    }
+    
+    reStartButton.classList.remove('hide');
+    scoreButton.classList.remove('hide');
+}
+
+reStartButton.onclick = () => {
+    paragraph.innerText = '最初はグー！って思うじゃん、けどそれってあなたの思いこみですよね？はい、じゃんけん・・・';
+
+    gameArea.classList.remove('hide');
+
+    imgうざおじ.src = 'img/うざおじ.png';
+    imgGu.classList.remove('hide');
+    imgChoki.classList.remove('hide');
+    imgPa.classList.remove('hide');
+
+    resultText.innerText = '';
+
+    reStartButton.classList.add('hide');
+    scoreButton.classList.add('hide');
+
+    endFlag=false;
+}
+
+scoreButton.onclick = () => {
+    let text = (win + lose + draw) + '戦: ';
+    text += win + '勝 ' + lose + '敗 ' + draw + '分\n\n';
+    for(let i=0;i<result.length;i++){
+        text += i+1+ '戦目 ' + winOrLose[result[i]];
+        text += ' [あなたの手：' + handArray[player[i]];
+        text += ', 相手の手：' + handArray[うざおじ[i]] + ']\n';
+    }
+    paragraph.innerText = text;
+
+    gameArea.classList.add('hide');
+    scoreButton.classList.add('hide');
+}
